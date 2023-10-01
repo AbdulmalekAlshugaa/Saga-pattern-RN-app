55ef75936d07b22da3e191f13ba0e7df992bafc9
Abdulmalik Alshugaa <alshugaa19@gmail.com>
2023-10-02 01:13:30 +0800

    feat(apiCall): remove some extra spacing as well as rename the hook

--- app/modules/product/hooks/useGetProductQuery.ts
+++ app/modules/product/hooks/useGetProductQuery.ts
@@ -2,11 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { ProductApiRoute } from '../src/productApi';
 import networkApi from '../../network/src/networkApi';
 
-
 export const useGetProducts = () => {
     const staleTime = 60 * 1000; // 1 minute
-    return useQuery([ProductApiRoute.GET_PRODUCTS], async () => {
-        const response = await networkApi.get(ProductApiRoute.GET_PRODUCTS);
+    return useQuery([ProductApiRoute.getProducts], async () => {
+        const response = await networkApi.get(ProductApiRoute.getProducts);
         if (!response.ok) {
             throw response;
         }
@@ -15,4 +14,3 @@ export const useGetProducts = () => {
         { cacheTime: staleTime, staleTime }
     );
 };
-// TODO: add types to response.
--- app/modules/product/src/productApi.ts
+++ app/modules/product/src/productApi.ts
@@ -1,6 +1,6 @@
 import networkApi from '../../network/src/networkApi';
 
 export const ProductApiRoute = {
-    GET_PRODUCTS: '/products',
+    getProducts: '/products',
 };
 
