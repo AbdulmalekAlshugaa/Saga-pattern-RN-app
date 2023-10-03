0a5fde7d3b88e3bd601294bb021c2f8e199b9fd1
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-10-03 13:53:27 +0800

    feat(task): adding navigation ref to the app navigation

---
+++ app/modules/navigation/RootNavigation.ts
@@ -0,0 +1,9 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+
+export const navigationRef = createNavigationContainerRef()
+
+export function navigateTo(name: string, params: any= {}): void {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as never, params as never)
+  }
+}
