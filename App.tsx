23c2871cf77d140915c90a7aa5f2cade9e3edd6f
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-10-03 13:14:02 +0800

    feat(feat): remove unused react native componenets

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import ProductListItem from './app/modules/product/view/ProductListItem';
-import ProductListItemScreen from './app/modules/product/view/ProductListItemScreen';
 import AppNavigation from './app/modules/main/navigation/mainAppNavigation';
 
 export default function App() {
