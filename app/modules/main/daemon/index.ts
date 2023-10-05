f4b047dbe63c2981c2eb8f9b54270e69546d4e5f
abdulmalik alshugaa <alshugaa19@gmail.com>
2023-10-05 19:21:49 +0300

    feat(remove): remvoe unused files as well as space

--- app/modules/main/daemon/index.ts
+++ app/modules/main/daemon/index.ts
@@ -2,6 +2,6 @@ import { all } from "redux-saga/effects";
 import { productMainRuntime } from "../../product/src/productSaga";
 
 export default function* rootSaga() {
-  // put all your sagas here in order to add more sagas in cases if we scall up our app 
+  // put all your sagas here in order to add more sagas in cases if we scale up our app 
   yield all([productMainRuntime()]);
 }
